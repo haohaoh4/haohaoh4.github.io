a27c73110f7da84673c04eb7ad5a21dca5039345
haohaoh4 <handahao@163.com>
2018-08-29 12:15:01 +0800

    Changes to be committed:

---
+++ self.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+<html>
+<body>
+
+<form action="self.php" method="post">
+Name: <input type="text" name="name" value=<?php echo $_POST["name"]; ?>><br>
+E-mail: <input type="text" name="email" value=<?php echo $_POST["email"]; ?>><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+</body>
+</html>
